1365750e636db72172f0b80a0fb193456a096107
Roger Toledo <roger.toledo@olxbr.com>
2025-03-20 08:36:21 -0300

    refactor: fix attribute tag

--- src/components/person/ModalPerson.tsx
+++ src/components/person/ModalPerson.tsx
@@ -1,48 +1,106 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { createPerson, getPerson, updatePerson } from '@/services/person';
 
-interface ModalProps {
-  isOpen: boolean;
-  onClose: () => void;
+interface ModalPersonProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onPersonAction: () => void;
+    isUpdate: boolean;
+    personId: string;
 }
 
-const ModalPerson: React.FC<ModalProps> = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+const ModalPerson: React.FC<ModalPersonProps> = ({ isOpen, onClose, onPersonAction, isUpdate, personId }) => {
+    const [personName, setPersonName] = useState('');
+    const [title, setTitle] = useState("Cadastro de Pessoa");
+    const [buttonText, setButtonText] = useState("Adicionar nova pessoa");
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
-        {/* Main modal */}
-        
+    useEffect(() => {
+        if (isUpdate && personId) {
+            setTitle("Atualização de Pessoa");
+            setButtonText("Atualizar pessoa");
+            const fetchPerson = async () => {
+                try {
+                    const response = await getPerson(personId);
+                    setPersonName(response.Message.name);
+                } catch (error) {
+                    console.error("Error fetching person", error);
+                }
+            };
+            fetchPerson();
+        } else {
+            setTitle("Cadastro de Pessoa");
+            setButtonText("Adicionar nova pessoa");
+            setPersonName("");
+        }
+    }, [isUpdate, personId]); // Removido personName
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            if (isUpdate) {
+                console.log("Updating person", personId, personName);
+                await updatePerson(personId, personName);
+            } else {
+                await createPerson(personName);
+            }
+
+            onPersonAction();
+        } catch (err) {
+            console.error("Error creating person", err);
+        }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPersonName(e.target.value);
+    };
+
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
             <div className="relative p-4 w-full max-w-md max-h-full">
-                {/*Modal content */}
                 <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-                    {/* Modal header */}
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                         <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                            Cadastro de Pessoa
+                            {title}
                         </h3>
                         <button type="button" onClick={onClose} className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l6-6"/>
                             </svg>
                             <span className="sr-only">Close modal</span>
                         </button>
                     </div>
-                    {/*  Modal body */}
                     <div className="p-4 md:p-5">
-                        <form className="space-y-4" action="#">
+                        <form onSubmit={handleSubmit} className="space-y-4">
                             <div>
                                 <label htmlFor="personName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
-                                <input type="text" name="personName" id="personName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nick Goose Bradshaw" required />
+                                <input
+                                    type="text"
+                                    name="personName"
+                                    id="personName"
+                                    value={personName}
+                                    onChange={handleChange}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                    placeholder="Digite um nome"
+                                    required
+                                />
                             </div>
-                            
-                            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Adicionar nova pessoa</button>
+                            <button
+                                type="submit"
+                                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                {buttonText}
+                            </button>
                         </form>
                     </div>
                 </div>
-        </div> 
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default ModalPerson;
--- src/components/purchase/ModalPurchase.tsx
+++ src/components/purchase/ModalPurchase.tsx
@@ -23,7 +23,7 @@ const ModalPurchaseType: React.FC<ModalProps> = ({ isOpen, onClose }) => {
                         </h3>
                         <button type="button" onClick={onClose} className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                             </svg>
                             <span className="sr-only">Close modal</span>
                         </button>
@@ -98,8 +98,13 @@ const ModalPurchaseType: React.FC<ModalProps> = ({ isOpen, onClose }) => {
                                 </div>
                             </div>
                             <div className="flex justify-center">
-                                <button type="submit" className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-30 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Adicionar nova compra</button>
-                            </div>
+    <button 
+        type="submit" 
+        className="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 sm:px-10 md:px-20 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-auto min-w-40"
+    >
+        Adicionar nova compra
+    </button>
+</div>
                         </form>
                     </div>
                 </div>
