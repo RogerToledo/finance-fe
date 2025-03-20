c065ebb7e85e2cf78f6685018d552b3941000692
Roger Toledo <roger.toledo@olxbr.com>
2025-03-20 18:03:16 -0300

    feat: add fields to credit card

--- src/components/creditCard/CreditCard.tsx
+++ src/components/creditCard/CreditCard.tsx
@@ -24,7 +24,16 @@ function CreditCard() {
                                 Id
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                Nome
+                                Proprietário
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Cartão Final
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Tido de Cartão
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Dia do Vencimento
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 
@@ -34,50 +43,110 @@ function CreditCard() {
                     <tbody>
                         <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                1
+                                fbc1b59d-a32d-4bf4-bfd4-6ebb575852e8
                             </th>
                             <td className="px-6 py-4">
                                 Teste 1
                             </td>
+                            <td className="px-6 py-4">
+                                0001
+                            </td>
+                            <td className="px-6 py-4">
+                                Físico
+                            </td>
+                            <td className="px-6 py-4">
+                                1
+                            </td>
                             <td className="px-6 py-4 text-right">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline ml-5">Deletar</a>
+                                <button 
+                                    type="button"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar
+                                </button>
+                                <button 
+                                    type="button" 
+                                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 me-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Deletar
+                                </button>
                             </td>
                         </tr>
                         <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                2
+                                fbc1b59d-a32d-4bf4-bfd4-6ebb575852e8
                             </th>
                             <td className="px-6 py-4">
                                 Teste 2
                             </td>
+                            <td className="px-6 py-4">
+                                0002
+                            </td>
+                            <td className="px-6 py-4">
+                                Físico
+                            </td>
+                            <td className="px-6 py-4">
+                                1
+                            </td>
                             <td className="px-6 py-4 text-right">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline ml-5">Deletar</a>
+                                <button 
+                                    type="button"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar
+                                </button>
+                                <button 
+                                    type="button" 
+                                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 me-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Deletar
+                                </button>
                             </td>
                         </tr>
                         <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                3
+                                fbc1b59d-a32d-4bf4-bfd4-6ebb575852e8
                             </th>
                             <td className="px-6 py-4">
                                 Teste 3
                             </td>
+                            <td className="px-6 py-4">
+                                0003
+                            </td>
+                            <td className="px-6 py-4">
+                                Virtual
+                            </td>
+                            <td className="px-6 py-4">
+                                1
+                            </td>
                             <td className="px-6 py-4 text-right">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline ml-5">Deletar</a>
+                                <button 
+                                    type="button"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar
+                                </button>
+                                <button 
+                                    type="button" 
+                                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 me-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Deletar
+                                </button>
                             </td>
                         </tr>
                         <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                4
+                                fbc1b59d-a32d-4bf4-bfd4-6ebb575852e8
                             </th>
                             <td className="px-6 py-4">
                                 Teste 4
                             </td>
+                            <td className="px-6 py-4">
+                                0004
+                            </td>
+                            <td className="px-6 py-4">
+                                Virtual Temporário
+                            </td>
+                            <td className="px-6 py-4">
+                                1
+                            </td>
                             <td className="px-6 py-4 text-right">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline ml-5">Deletar</a>
+                                <button 
+                                    type="button"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar
+                                </button>
+                                <button 
+                                    type="button" 
+                                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 me-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Deletar
+                                </button>
                             </td>
                         </tr>
                     </tbody>
--- src/components/creditCard/ModalCreditCard.tsx
+++ src/components/creditCard/ModalCreditCard.tsx
@@ -36,7 +36,20 @@ const ModalcreditCard: React.FC<ModalProps> = ({ isOpen, onClose }) => {
                                 <input type="text" name="Owner" id="Owner" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nick Goose Bradshaw" required />
                             </div>
                             <div>
-                                <label htmlFor="InvoiceClosingDay" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dia do fechamento da Fatura</label>
+                                <label htmlFor="InvoiceClosingDay" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Final do Cartão</label>
+                                <input type="text" name="InvoiceClosingDay" id="InvoiceClosingDay" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="15" required />
+                            </div>
+                            <div>
+                            <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo do Cartão</label>
+                            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected>Escolha o tipo do cartão</option>
+                                <option value="F">Físico</option>
+                                <option value="V">Virtual</option>
+                                <option value="VT">Virtual Temporário</option>
+                            </select>
+                            </div>
+                            <div>
+                                <label htmlFor="InvoiceClosingDay" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dia do Fechamento da Fatura</label>
                                 <input type="text" name="InvoiceClosingDay" id="InvoiceClosingDay" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="15" required />
                             </div>
                             
